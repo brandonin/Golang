4ed3bda93c485ab327f36af45c911bbd1091ce06
brandon in <brandonin@me.com>
2016-04-27 16:31:14 -0700

    learning how to use GO

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+// import (
+// 	"fmt"
+// 	"os"
+// )
+// func main() {
+// 	if len(os.Args) != 2 {
+// 		os.Exit(1)
+// 	}
+// 	fmt.Println("It's over", os.Args[1])
+
+// 	// power := 9000
+// 	// fmt.Printf("It's over %d\n", power)
+// 	power := getPower()
+// }
+
+// func getPower() int {
+// 	return 9001
+// }
+
+func main() {
+	power := 9000
+}
