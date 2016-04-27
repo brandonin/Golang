baaf87dd38b65b0fe9a60f3ef99d43b99e004bb1
brandon in <brandonin@me.com>
2016-04-27 16:43:24 -0700

    cannot have unused variables

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
-// import (
-// 	"fmt"
-// 	"os"
-// )
+import (
+	"fmt"
+	// "os"
+)
 // func main() {
 // 	if len(os.Args) != 2 {
 // 		os.Exit(1)
@@ -19,6 +19,38 @@ package main
 // 	return 9001
 // }
 
-func main() {
-	power := 9000
-}
+
+// Can only declar variable once example: so you must use = instead of := in the second indication of power
+
+// func main() {
+// 	power := 9000
+// 	fmt.Printf("It's over %d\n", power)
+
+// 	power = 9001
+// 	fmt.Printf("It's also over %d\n", power)
+// }
+
+
+// you can declare two variables with := on the left hand side
+
+// func main() {
+// 	name, power := "Goku", 9000
+// 	fmt.Printf("%s's power is over %d\n", name, power)
+// }
+
+// You can redeclare a variable with := as long as their is one undeclared variable on the left hand side
+
+// func main() {
+// 	power := 1000
+// 	fmt.Printf("default power is %d\n", power)
+
+// 	name, power := "Goku", 9000
+// 	fmt.Printf("%s's power is over %d\n", name, power)
+// }
+
+// Go doesn't allow unused variables
+
+// func main() {
+// 	name, power := "Goku", 1000
+// 	fmt.Printf("default power is %d\n", power)
+// }
